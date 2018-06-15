aae35f374aab384281b254781be5218008da3284
= <=>
2018-06-15 10:02:57 +0200

    Example of order of execution by Spring. See output in SetterDemoApp

--- src/eva/luv2code/springdemo/CricketCoach.java
+++ src/eva/luv2code/springdemo/CricketCoach.java
@@ -3,6 +3,10 @@ package eva.luv2code.springdemo;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
+	private String email;
+	private String team;
+	
+	
 
 	/*The sysouts demonstrate that Spring calls the setter and constructor also when the bean is
  	only called by getBean(), but no methods are executed upon it.
@@ -13,28 +17,51 @@ public class CricketCoach implements Coach {
 	
 	*/
 	public CricketCoach() {
-		System.out.println("CricketCoach: inside no-arg constructor");
+		System.out.println("Inside no-arg constructor from CricketCoach");
 	}
 	
 	
 	//Setter will be called by Spring when adding the dependency
 	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("CricketCoach: inside setter method setFortuneService");
+		System.out.println("Inside setter method setFortuneService from CricketCoach");
 		this.fortuneService = fortuneService;
 	}
 	
 	
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		System.out.println("Inside setEmail() from CricketCoach");
+		this.email = email;
+	}
+
+
+	public String getTeam() {
+		return team;
+	}
+
+
+	public void setTeam(String team) {
+		System.out.println("Inside setTeam() from CricketCoach");
+
+		this.team = team;
+	}
+
+
 	@Override
 	public String getDailyWorkOut() {
-		System.out.println("Do some crickety crickety");
-		return "DahmeenteGyesnie";
+		System.out.println("Inside getDailyWorkOut(): Do some crickety crickety");
+		return "Return value getDailyWorkOut inside CricketCoach";
 	}
 
 	
 	@Override
 	public String getDailyFortune() {
-		System.out.println("snaptegeitnog");
-		return fortuneService.getFortune();
+		System.out.println("Inside getDailyFortune() from CricketCoach");
+		return "Return value for getDailyFortune : " + fortuneService.getFortune();
 	}
 
 	
--- src/eva/luv2code/springdemo/SetterDemoApp.java
+++ src/eva/luv2code/springdemo/SetterDemoApp.java
@@ -14,10 +14,27 @@ public class SetterDemoApp {
 		
 		//call methods on the bean
 		
-		System.out.println(theCoach.getDailyFortune()+" \n" + theCoach.getDailyWorkOut());
+		System.out.println(theCoach.getDailyFortune()+"\n" + theCoach.getDailyWorkOut());
+		
+		//call new methods to get the literal values
+		System.out.println(theCoach.getEmail() + "\n" + theCoach.getTeam());
 		
 		//close the context
 		context.close();
+		
+		
+		/* OUTPUT:																LOCATION:
+			Inside no-arg constructor from CricketCoach							applicationContext	
+			Inside setter method setFortuneService from CricketCoach			applicationContext set up setter injection
+			Inside setEmail() from CricketCoach									applicationContext inject literal value
+			Inside setTeam() from CricketCoach									applicationContext inject literal value
+			Inside getDailyFortune() from CricketCoach							CricketCoach gettter
+			Inside getDailyWorkOut(): Do some crickety crickety					CricketCoach getter
+			Return value for getDailyFortune : Today is you happylucky day		getter return value
+			Return value getDailyWorkOut inside CricketCoach					getter return value
+			eva@luvs2code.nl													value in applicationContext
+			The WhoopWhoops														value in applicationContext
+			*/
 	}
 
 }
